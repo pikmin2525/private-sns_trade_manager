1f191e368297db3e863995c3b6181b05ec291a45
pikmin2525 <ayaneosi15@gmail.com>
2026-08-28 02:44:51 +0900

    m_contents カラム名`service_name`を`content_name`に修正

--- src/database/tables/m_contents.ts
+++ src/database/tables/m_contents.ts
@@ -3,7 +3,7 @@ import { baseColumns } from "../baseColumns.js";
 export const createContentsTable = `
 CREATE TABLE IF NOT EXISTS m_contents (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    service_name CHAR(256) NOT NULL,
+    content_name CHAR(256) NOT NULL,
     ${baseColumns}
 );
 `;
