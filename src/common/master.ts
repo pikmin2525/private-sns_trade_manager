01c39874e4da96666c3a0186a4d7ac04a50776e9
pikmin2525 <ayaneosi15@gmail.com>
2026-08-28 03:45:57 +0900

    初期マスタデータ作成

---
+++ src/common/master.ts
@@ -0,0 +1,30 @@
+type socialServicesList = {
+    service_name: string;
+    domain: string;
+};
+export const socialServicesList = [
+    {
+        service_name: "X",
+        domain: "x.com",
+    },
+    {
+        service_name: "Instagram",
+        domain: "instagram.com",
+    },
+];
+
+
+type contentsList = {
+    content_name: string;
+};
+export const contentsList = [
+    {
+        content_name: "Dr.STONE",
+    },
+    {
+        content_name: "ツイステッドワンダーランド",
+    },
+    {
+        content_name: "ハイキュー!!",
+    },
+];
